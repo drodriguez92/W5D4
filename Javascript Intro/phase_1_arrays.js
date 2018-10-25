501427f5c97dd6c8eb76d724913c9417b32851cc
Diego <diego.rodriguez923@gmail.com>
2018-10-25 17:45:12 -0400

    Add my_uniq & my_twosum w/ comments

---
+++ Javascript Intro/phase_1_arrays.js
@@ -0,0 +1,25 @@
+Array.prototype.my_uniq = function() {
+  var result = [];                              // Set up result array.
+
+  for (var i = 0; i < this.length; i+= 1) {     // Iterate from start to finish by 1.
+    if (result.indexOf(this[i]) === -1) {       // Checks if element doesn't exist in result.
+      result.push(this[i]);                     // Adds it if it doesn't.
+    }
+  }
+
+  return result;                                // Returns given array unique'd.
+};
+
+Array.prototype.my_twoSum = function() {
+  var pairs = [];                                       // Set up pairs array.
+
+  for (var i = 0; i < this.length; i += 1) {            // Create outer loop and initialize i.
+    for (var j = (i + 1); j < this.length; j += 1) {    // Create inner loop and initialize j.
+      if (this[i] + this[j] === 0) {                    // Checks if i + j = 0
+        pairs.push([i, j]);                             // Adds i and j into pairs array.
+      }
+    }
+  }
+
+  return pairs;                                         // Returns all possible pairs of nums that sum to 0. 
+};
